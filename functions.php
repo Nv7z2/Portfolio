db98242bde4333fe332a330cd3f0da34f1e914d9
Kacper Baran <barankacper2000@gmail.com>
2024-01-14 21:44:07 +0100

    feat: disable anime.js stuff and unnecessary heading formatting js

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('theme-styles', get_template_directory_uri() . '/assets/dist/css/style.min.css', array(), '1.0', 'all');
     
     wp_enqueue_script('anime', get_template_directory_uri() . '/assets/dist/js/anime.min.js', array(), '1.0', true);
-    wp_enqueue_script('heading-formatting', get_template_directory_uri() . '/assets/dist/js/heading-formatting.min.js', array(), '1.0', true);
-    wp_enqueue_script('animations', get_template_directory_uri() . '/assets/dist/js/animations.min.js', array('anime'), '1.0', true);
+    // wp_enqueue_script('heading-formatting', get_template_directory_uri() . '/assets/dist/js/heading-formatting.min.js', array(), '1.0', true);
+    // wp_enqueue_script('animations', get_template_directory_uri() . '/assets/dist/js/animations.min.js', array('anime'), '1.0', true);
 });
 
 // Add missing menu option to wp-admin
